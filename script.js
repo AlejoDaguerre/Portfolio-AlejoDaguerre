d8c7d5a788cf3ebce7beecb82f2e9c45cd84bf64
Alejo Daguerre <104533078+AlejoDaguerre@users.noreply.github.com>
2023-03-20 19:30:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let menuVisible = false;
+
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+window.onscroll = function(){
+    efectoHabilidades();
+} 
